d003c28bd02cf7a49dbc74176c27450b83a9674c
Coltron4 <cmp009@email.uark.edu>
2014-10-06 18:57:55 -0500

    Started Android Project
    Created the project for our application. I created three of the
    activities from Gage's mockup: Summary, Account, and Settings. These are
    the only three that can navigate to each other. I added list views to
    them as well. We need to throw in some faux data in these views to make
    the UI look better for our presentation.

---
+++ DinoGame/src/edu/uark/csce/mobile/dinogame/SummaryActivity.java
@@ -0,0 +1,47 @@
+package edu.uark.csce.mobile.dinogame;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SummaryActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_summary);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.summary, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	// Button listeners
+	public void viewAccount(View v) {
+		Intent intent = new Intent(SummaryActivity.this, AccountActivity.class);
+		startActivity(intent);
+	}
+	
+	public void viewSettings(View v) {
+		Intent intent = new Intent(SummaryActivity.this, SettingsActivity.class);
+		startActivity(intent);
+	}
+}
