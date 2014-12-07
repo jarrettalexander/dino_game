7bf785ea3935a4d4c2373186e04c8ce64c9168df
Coltron4 <cmp009@email.uark.edu>
2014-12-06 23:39:38 -0600

    Delete Dino
    Added delete dinosaur functionality. Also added dialog box to verify the
    deletion.

--- DinoGame/src/edu/uark/csce/mobile/dinogame/CharacterActivity.java
+++ DinoGame/src/edu/uark/csce/mobile/dinogame/CharacterActivity.java
@@ -8,13 +8,20 @@ import java.util.ArrayList;
 import java.util.List;
 import com.google.android.gms.maps.model.LatLng;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class CharacterActivity extends Activity {
+public class CharacterActivity extends Activity implements DeleteDinoDialogFragment.DeleteDinoDialogListener {
 	
 	// Database
 	private DinosDataSource datasource;
@@ -25,13 +32,16 @@ public class CharacterActivity extends Activity {
 	private DinoItem dino;
 	private ArrayList<Integer> stats;
 	private boolean equipped;
+	private int[] color = {0, 0, 0};
 	
-	// Textviews
+	// Info Views
 	private TextView nameText;
 	private TextView attackText;
 	private TextView defenseText;
 	private TextView specialText;
 	private TextView equipText;
+	private ProgressBar expBar;
+	private ImageView dinoPic;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -56,7 +66,7 @@ public class CharacterActivity extends Activity {
 		if(dino.getmEquip() == -1)
 			equipped = false;
 		
-		// Adjust TextViews based on data
+		// Adjust Views based on data
 		nameText = (TextView)findViewById(R.id.textView2);
 		nameText.setText(dino.getmName());
 		attackText = (TextView)findViewById(R.id.textView4);
@@ -70,6 +80,43 @@ public class CharacterActivity extends Activity {
 			equipText.setText("a hat");
 			// TODO Retrieve item information and display it
 		}
+		expBar = (ProgressBar)findViewById(R.id.expBar);
+		expBar.setProgress(dino.getmExperience());
+		if(dino.getmColor() == 1)
+			color[0] = 204;
+		else if(dino.getmColor() == 2)
+			color[1] = 255;
+		else if(dino.getmColor() == 3)
+			color[2] = 255;
+		
+		// Adjust dino color
+		dinoPic = (ImageView)findViewById(R.id.dinosaur);
+		Drawable d = getResources().getDrawable(R.drawable.dinosaur_bigger);
+	    Bitmap bp = ((BitmapDrawable)d).getBitmap();
+	    int color2 = Color.argb(255, 255, 255, 255);
+	    /*for(int i = 1; i < bp.getHeight(); i++) {
+	    	for(int j = 1; j < bp.getWidth(); j++) {
+	    		if(bp.getPixel(i, j) == color2)
+	    			bp.setPixel(i, j, Color.argb(255, color[0], color[1], color[2]));
+	    	}
+	    }*/
+	    
+//	    bp.setPixel(10, 10, Color.argb(255, color[0], color[1], color[2]));
+//	    bp.setPixel(11, 10, Color.argb(255, color[0], color[1], color[2]));
+//	    
+	    /*bp.setPixel(0, 0, Color.rgb(195, 195, 195));
+	    int color1 = bp.getPixel(10, 10);
+	    int color2 = Color.argb(255, 255, 255, 255);
+	    int color3 = bp.getPixel(11, 10);
+	    int alpha1 = Color.red(color1);
+	    int alpha2 = Color.red(color2);
+	    int alpha3 = Color.red(color3);
+	    Log.e("alpha values", "alpha1 = " + alpha1 + ", alpha2 = " + alpha2 + ", alpha3 = " + alpha3);
+	    if(color1 == color2) {
+	    	Log.e("pixel test", "It's a match!!!"); }
+	    if(color1 == color3) {
+	    	Log.e("color test", "Matches itself!!!");
+	    }*/
 	}
 	
 	// Button listeners
@@ -93,6 +140,10 @@ public class CharacterActivity extends Activity {
 		startActivity(intent);
 	}
 	
+	public void deleteDino(View v) {
+		showDeleteDialog();
+	}
+	
 	// Converts byte arrays for latitudes and longitudes to array lists
 	public void convertBytes(byte[] bytStats) throws IOException {
 
@@ -105,5 +156,29 @@ public class CharacterActivity extends Activity {
 		}
 
 	}
+	
+	// Dialog functions
+	public void showDeleteDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new DeleteDinoDialogFragment();
+        dialog.show(getFragmentManager(), "NoticeDialogFragment");
+    }
+
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following methods
+    // defined by the NoticeDialogFragment.NoticeDialogListener interface
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
+    	Intent intent = new Intent(CharacterActivity.this, SummaryActivity.class);		
+    	datasource.deleteDino(dino);
+		startActivity(intent);
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
+        // Nothing needs to be done
+    }
 
 }
---
+++ DinoGame/src/edu/uark/csce/mobile/dinogame/DeleteDinoDialogFragment.java
@@ -0,0 +1,63 @@
+package edu.uark.csce.mobile.dinogame;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class DeleteDinoDialogFragment extends DialogFragment {
+	
+	private final String title = "Delete Dino";
+	private final String message = "Are you sure you want to do this? Your dinosaur will die and never get to" +
+			" fight again...";
+	
+	/* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface DeleteDinoDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+    
+    // Use this instance of the interface to deliver action events
+    DeleteDinoDialogListener mListener;
+    
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (DeleteDinoDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+    
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Build the dialog and set up the button click handlers
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title)
+        	   .setMessage(message)
+               .setPositiveButton("Do it.", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // Send the positive button event back to the host activity
+                       mListener.onDialogPositiveClick(DeleteDinoDialogFragment.this);
+                   }
+               })
+               .setNegativeButton("Oh no stop!", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // Send the negative button event back to the host activity
+                       mListener.onDialogNegativeClick(DeleteDinoDialogFragment.this);
+                   }
+               });
+        return builder.create();
+    }
+
+}
